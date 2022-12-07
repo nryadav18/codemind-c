7e49ac1e405b85c1fbcdfda5cc22bcbcfd5af4ba
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2022-12-07 11:37:13 +0530

    #include<stdio.h> #include<math.h> int main () {     int a,b,c,d;     scanf("%d%d%d%d",&a,&b,&c,&d);     float dis;     dis=sqrt(pow((a-c),2)+pow((b-d),2));     printf("%.4f",dis); }

---
+++ distance_between_the_two_points.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    float dis;
+    dis=sqrt(pow((a-c),2)+pow((b-d),2));
+    printf("%.4f",dis);
+}
