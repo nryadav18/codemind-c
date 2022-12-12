c625803f8ee9f19b50f735dfd9363332480716ac
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2022-12-12 11:13:07 +0530

    #include<stdio.h> int main () {     int n;     scanf("%d",&n);     int a[n],s=0;     for (int i=0;i<n;i++)     {         scanf("%d",&a[i]);         s=s+a[i];     }     printf("%d",s); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],s=0;
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        s=s+a[i];
+    }
+    printf("%d",s);
+}
