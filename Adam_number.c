d7fadc73413db42e10d7863e6c8a57fc5f680781
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2022-12-18 15:03:37 +0530

    #include<stdio.h> int funa(int a)  //441 {     int b;     b=(a*a);     return(b); } int funb(int a) {     int b=a;     int r,s=0;     while (b!=0)     {         r=b%10;         s=s*10+r;         b=b/10;     }     int sq=(s*s);     int r1,s1=0;     while(sq!=0)     {         r1=sq%10;         s1=s1*10+r1;         sq=sq/10;     }     return(s1); } int main () {     int a;     scanf("%d",&a);     int x,y;     x=funa(a);     y=funb(a);     if (x==y) printf("True");     else printf("False"); }

---
+++ Adam_number.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int funa(int a)  //441
+{
+    int b;
+    b=(a*a);
+    return(b);
+}
+int funb(int a)
+{
+    int b=a;
+    int r,s=0;
+    while (b!=0)
+    {
+        r=b%10;
+        s=s*10+r;
+        b=b/10;
+    }
+    int sq=(s*s);
+    int r1,s1=0;
+    while(sq!=0)
+    {
+        r1=sq%10;
+        s1=s1*10+r1;
+        sq=sq/10;
+    }
+    return(s1);
+}
+int main ()
+{
+    int a;
+    scanf("%d",&a);
+    int x,y;
+    x=funa(a);
+    y=funb(a);
+    if (x==y) printf("True");
+    else printf("False");
+}
