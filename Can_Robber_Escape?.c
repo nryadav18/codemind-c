218fae1ce55315cb5b5f9706928f50943cbe47f1
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-01-24 19:35:25 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==1)         {             c++;         }     }     if(c<=2)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==1)
+        {
+            c++;
+        }
+    }
+    if(c<=2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
